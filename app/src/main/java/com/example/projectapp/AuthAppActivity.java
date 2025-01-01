118e1c573488f1cc7580315d7f317624934f3e63
Ali-codingg <alikheireddinee@outlook.com>
2025-01-01 21:23:15 +0200

    Fix BottomNavigationMenu and Design HomeActivity

--- app/src/main/java/com/example/projectapp/AuthAppActivity.java
+++ app/src/main/java/com/example/projectapp/AuthAppActivity.java
@@ -1,12 +1,19 @@
 package com.example.projectapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AuthAppActivity extends AppCompatActivity {
 
@@ -15,10 +22,12 @@ public class AuthAppActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        replaceFragment(new HomeFragment());
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.scroll_view, fragment);
+        fragmentTransaction.commit();
     }
 }
